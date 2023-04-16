778598d705e28a046ecef20a0237aadc7140666e
PritamReddy101 <Pritam.nandikonda@gmail.com>
2023-04-17 02:07:08 +0530

    singleBlog page changes

--- src/single-blog/SingleBlog.js
+++ src/single-blog/SingleBlog.js
@@ -1,57 +1,74 @@
 import './singleBlog.css';
 
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useState, useEffect } from 'react';
 import ArrowBackIos from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
+
+import {db} from './firebase';
+
 function SingleBlog() {
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const getPostsFromFirebase = [];
+    const subscriber = db
+      .collection("Blogs")
+      .onSnapshot((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          getPostsFromFirebase.push({
+            ...doc.data(), //spread operator
+            key: doc.id, // `id` given to us by Firebase
+          });
+        });
+        setPosts(getPostsFromFirebase);
+        setLoading(false);
+      });
+
+    // return cleanup function
+    return () => subscriber();
+  }, [loading]); // empty dependencies array => useEffect only called once
+
+  if (loading) {
+    return <h1>loading firebase data...</h1>;
+  }
+  console.log(posts);
+
+  const goToPrev = () => {
+    const isFirstPost = currentIndex === 0;
+    const newIndex = isFirstPost ? posts.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastPost = currentIndex === posts.length - 1;
+    const newIndex = isLastPost ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+  
+  const markup = { __html: posts[currentIndex].Content};
+  
+
   return (
     <div className="blog-page">
     <div className="navbar"><h1>Navbar</h1></div>
-    <div className="blog-heading"><h1>BLOGS</h1></div>
+    <div className="blog-heading"><h1 className="blog_h1">BLOGS</h1></div>
     <div className="blog-card">
-      <h2>BLOG TITLE</h2>
-    <Card sx={{ maxWidth: 645 }}>
-      <CardMedia
-        sx={{ height: 340 }}
-        image="/blog.jpeg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        The Beginning Of The Cosmic Saga
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Aftermath of The Big Bang
-        It all started 13.82 billion years ago when the universe we know started inflating from a tiny singularity faster than light itself and has been expanding since 
-        (That does not compromise Albert Einstein’s speed limit, since the highest speed that anything can reach within the universe is that of light.
-        It was not the case for the expansion of the universe itself).
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+      {/* <h2>BLOG TITLE</h2> */}
+      <div dangerouslySetInnerHTML={markup} />;
     </div>
     <div className="blog-navigate">
-      {/* <div className="dummy"> */}
       <div className="blog-prev">
         <div><ArrowBackIos></ArrowBackIos></div>
-        <div>Prev Blog</div>
+        <div>Previous Blog</div>
       </div>
       <div className="blog-next">
         <div>Next Blog</div>
         <div><ArrowForwardIosIcon></ArrowForwardIosIcon></div>
       </div>
-      {/* </div> */}
     </div>
     </div>
   );
