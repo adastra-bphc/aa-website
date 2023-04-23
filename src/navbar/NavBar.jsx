099911f93b852ee4eda256f35b5a72a7a8ec6bc9
Abhiinv <abhiinv24@gmail.com>
2023-04-23 15:44:34 +0530

    Restructure Home and Add Links

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,30 +1,33 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import './NavBar.css'
+import logo from './AdAstra.png'
 
 export default function NavBar() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-dark bg-body-tertiary text-white">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <img src={logo} alt="..." class="listimg-item" />
+                <button class="navbar-toggler text-white " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="collapse navbar-collapse text-white" id="navbarSupportedContent">
                     <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <li class="nav-item list-item">
+                            <Link to="/" class="nav-link active text-white" aria-current="page">Home</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">About Us</a>
+                        <li class="nav-item list-item">
+                            <Link to="/about-us" class="nav-link active text-white">About Us</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Blogs</a>
+                        <li class="nav-item list-item">
+                            <Link to="/blogs" class="nav-link active text-white">Blogs</Link>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Photography</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Spaced Out</a>
-                        </li>
+                        {/* <li class="nav-item list-item">
+          <a class="nav-link active text-white" href="#">Photography</a>
+        </li>
+        <li class="nav-item list-item">
+          <a class="nav-link active text-white" href="#">Spaced Out</a>
+        </li> */}
                     </ul>
                 </div>
             </div>
